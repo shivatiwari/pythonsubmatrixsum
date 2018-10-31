0137cac915f517748bab90edb965ea3a64c06afa
Shivansh Tiwari <shivansh22t@gmail.com>
2018-10-31 23:35:44 +0530

    Update sumofsubmat.py

--- sumofsubmat.py
+++ sumofsubmat.py
@@ -1,16 +1,15 @@
 ls = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
 
 def sumofsubm(n,k):
-    if k > len(n):
-        return
-    else:
-        for i in range(n-k+1):
-            for j in range(n-k+1):
-                sum = 0
-                for p in range(i,k+i):
-                    for q in range(j,k+j):
-                        sum = sum + ls[p][q]
-                print(sum, " ")
-        print()
+    a = len(n)-(k)
+    for i in range(a+1):
+        for j in range(a+1):
+            sum = 0
+            for p in range(i,k+i):
+                for q in range(j,k+j):
+                    print(p,q)
+                    sum = sum + ls[p][q]
+            print(sum, " ")
+    print(a)
         
 sumofsubm(ls,2)
